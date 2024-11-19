03dd458c5e4cd53d613e641a1ebf9d590cd576b3
Andrea Mazzullo <mazzulloandrea@gmail.com>
2024-11-19 15:15:10 +0100

    In order to use SSR use getServerSideProps instead getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,72 @@
 import Head from "next/head";
 
-
 import {
-  useStoryblokState,
-  getStoryblokApi,
-  StoryblokComponent,
+	useStoryblokState,
+	getStoryblokApi,
+	StoryblokComponent,
 } from "@storyblok/react";
 
 export default function Home({ story }) {
-  story = useStoryblokState(story);
+	story = useStoryblokState(story);
 
-  return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <StoryblokComponent blok={story.content} />
-    </div>
-  );
+	return (
+		<div>
+			<Head>
+				<title>Create Next App</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<header>
+				<h1>{story ? story.name : "My Site"}</h1>
+			</header>
+
+			<StoryblokComponent blok={story.content} />
+		</div>
+	);
 }
 
-export async function getStaticProps() {
-  let slug = "home";
+// export async function getStaticProps() {
+// 	// let slug = "home";
+// 	let slug = "newcontentstory";
 
-  let sbParams = {
-    version: "draft", // or 'published'
-  };
+// 	let sbParams = {
+// 		version: "draft", // or 'published'
+// 	};
 
-  const storyblokApi = getStoryblokApi();
-  let { data } = await storyblokApi.get(`cdn/stories/${slug}`, sbParams);
+// 	const storyblokApi = getStoryblokApi();
+// 	let { data } = await storyblokApi.get(`cdn/stories/${slug}`, sbParams);
 
-  return {
-    props: {
-      story: data ? data.story : false,
-      key: data ? data.story.id : false,
-    },
-    revalidate: 3600,
-  };
+// 	return {
+// 		props: {
+// 			story: data ? data.story : false,
+// 			key: data ? data.story.id : false,
+// 		},
+// 		revalidate: 3600,
+// 	};
+// }
+
+// serve rside rendering
+export async function getServerSideProps(context) {
+	// get the query object
+	const insideStoryblok = context.query._storyblok;
+
+	// let slug = "home";
+	let slug = "newcontentstory";
+
+	let sbParams = {
+		version: "draft", // or 'draft'
+	};
+
+	if (insideStoryblok) {
+		sbParams.version = "draft";
+	}
+
+	const storyblokApi = getStoryblokApi();
+	let { data } = await storyblokApi.get(`cdn/stories/${slug}`, sbParams);
+
+	return {
+		props: {
+			story: data ? data.story : false,
+			key: data ? data.story.id : false,
+		},
+	};
 }
